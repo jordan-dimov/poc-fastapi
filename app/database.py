236602dbefbcd757260aa19e70df9dd7606969a3
Jordan Dimov <jdimov@a115.co.uk>
2021-03-03 12:14:48 +0000

    Updates requirements and adds DB configuration for Aurora

--- app/database.py
+++ app/database.py
@@ -5,8 +5,16 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 SQLALCHEMY_DATABASE_URL = os.environ["DB_URI"]
+cluster_arn = os.environ["DB_CLUSTER_ARN"]
+secret_arn = os.environ["DB_SECRET_ARN"]
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={})
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, 
+    connect_args={
+        'aurora_cluster_arn': cluster_arn,
+        'secret_arn': secret_arn,
+    }
+)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
